1c90ad36da25003d2c07b21dc82cdf0ffbea67b1
void <infinitevoid424@protonmail.com>
2022-11-13 07:47:13 +0800

    added words

--- CLIscript.py
+++ CLIscript.py
@@ -1,3 +1,7 @@
 # The main script for the file
-
+import innerlib
 directory = ''
+
+if __name__ == "__main__":
+    print('\n Hello!')
+    print('\n Please type or paste the file directory of the files you are trying to convert.')
--- innerlib.py
+++ innerlib.py
@@ -1,8 +1,28 @@
 # Library containing the core functions
 import ffmpy
+import pathlib 
+
+# ffmpy is a library that does stuff that ffmpeg does
+# pathlib will be used to help standardize stuff and check if the directory exists
+
+
+def convert_dir(directory, target, source):
+    print('test')
+
 # source - the extensions that will be converted
 # target - the target file types that will be converted to
 # directory - the directory which will be converted
-def default_run(directory, target, source):
-    print('test')
+# Returns 0 on Success
 
+
+def verify(directory) -> bool:
+    return True
+    return False
+
+# Returns true if the directory exists
+# Returns false if it is not found
+
+def return_files(directory, extension) -> list:
+    file_list = []
+    return file_list
+# Returns all file paths of files which has the extension given.
